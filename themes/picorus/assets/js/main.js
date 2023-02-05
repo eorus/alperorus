bef8d2ac38f62912ad013face2b57de8e0d545b3
Alper Orus <alperor@gmail.com>
2023-02-05 19:23:02 +0300

    upd

--- themes/picorus/assets/js/main.js
+++ themes/picorus/assets/js/main.js
@@ -1,33 +1,38 @@
-/****
-Color Toggle
-*****/
 
-window.console = window.console || function(t) {};
-if (document.location.search.match(/type=embed/gi)) {
-  window.parent.postMessage("resize", "*");
-}
-console.log('Ka.');
-const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+// Get the theme toggle input
+const themeToggle = document.querySelector(
+  '.theme-switch input[type="checkbox"]'
+);
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark'); //add this
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
-}
-toggleSwitch.addEventListener('change', switchTheme, false);
+// Get the current theme from local storage
+const currentTheme = localStorage.getItem("theme");
 
-const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
+// If the current local storage item can be found
 if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+  // Set the body data-theme attribute to match the local storage item
+  document.documentElement.setAttribute("data-theme", currentTheme);
 
-    if (currentTheme === 'dark') {
-        toggleSwitch.checked = true;
-    }
+  // If the current theme is dark, check the theme toggle
+  if (currentTheme === "dark") {
+    themeToggle.checked = true;
+  }
 }
+
+// Function that will switch the theme based on the if the theme toggle is checked or not
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+// Add an event listener to the theme toggle, which will switch the theme
+themeToggle.addEventListener("change", switchTheme, false);
+
+
 
 /*!
  * Minimal theme switcher
